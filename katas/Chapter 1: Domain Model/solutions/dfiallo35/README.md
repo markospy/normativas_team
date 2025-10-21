83bbee588d6ef09049cdaf3b2ef606a38bf024e4
Dennis Fiallo <dfiallo35@gmail.com>
2025-10-21 15:29:09 -0400

    add domain layer kata

---
+++ katas/Chapter 1: Domain Model/solutions/dfiallo35/README.md
@@ -0,0 +1,3 @@
+## How to run
+
+Use pytest to run the tests.
---
+++ katas/Chapter 1: Domain Model/solutions/dfiallo35/models.py
@@ -0,0 +1,42 @@
+import datetime
+
+from exceptions import NoAvailableVet
+
+class BaseEntity:
+    def model_dump(self):
+        return self.__dict__
+
+
+class AppointmentRequest(BaseEntity):
+    def __init__(self, client_name: str, pet_name: str, specialty: str, date: datetime.date):
+        super().__init__()
+        self.client_name = client_name
+        self.pet_name = pet_name
+        self.specialty = specialty
+        self.date = date
+
+
+class Veterinarian(BaseEntity):
+    def __init__(self, name: str, specialty: str, max_daily_appointments: int):
+        super().__init__()
+        self.name = name
+        self.specialty = specialty
+        self.max_daily_appointments = max_daily_appointments
+        self._appointments = set()
+
+    def can_accept(self, appointment: AppointmentRequest) -> bool:
+        return appointment.specialty == self.specialty and len(self._appointments) < self.max_daily_appointments
+    
+    def assign(self, appointment: AppointmentRequest) -> True:
+        if self.can_accept(appointment):
+            self._appointments.add(appointment)
+            return True
+        
+        return False
+
+
+def allocate_appointment(appointment, veterinarians) -> None:
+    for vet in sorted(veterinarians, key=lambda v: len(v._appointments)):
+        if vet.assign(appointment):
+            return
+    return NoAvailableVet("No available vet")
